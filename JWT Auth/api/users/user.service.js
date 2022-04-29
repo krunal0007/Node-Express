0b31509f27f78e2aef35ffc708cf82c4a972ac00
krunal <vagadiyakrunal077@gmail.com>
2022-04-29 11:24:13 +0530

    first JWT commit

---
+++ JWT Auth/api/users/user.service.js
@@ -0,0 +1,91 @@
+const pool = require("../../config/database");
+
+module.exports = {
+  create: (data, callback) => {
+    pool.query(
+      `insert into crud1(firstName, lastName, gender, email, password, number) values (?,?,?,?,?,?)`,
+      [
+        data.first_name,
+        data.last_name,
+        data.gender,
+        data.email,
+        data.password,
+        data.number,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, results);
+      }
+    );
+  },
+  getUsers: (callBack) => {
+    pool.query(
+      `select id,firstName,lastName,gender,email,number from crud1`,
+      [],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  getUserByUserId: (id, callBack) => {
+    pool.query(
+      `select id,firstName,lastName,gender,email,number from crud1 where id = ?`,
+      [id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  updateUser: (data, callBack) => {
+    pool.query(
+      `update crud1 set firstName=?, lastName=?, gender=?, email=?, password=?, number=? where id = ?`,
+      [
+        data.first_name,
+        data.last_name,
+        data.gender,
+        data.email,
+        data.password,
+        data.number,
+        data.id,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  deleteUser: (data, callBack) => {
+    pool.query(
+      `delete from crud1 where id = ?`,
+      [data.id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  getUserByUserEmail: (email, callBack) => {
+    pool.query(
+      `select * from crud1 where email = ?`,
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+};
